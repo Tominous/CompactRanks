707c8bd73e5ca0de574bf81b718f301bec02b396
IconPippi <39626081+IconPippi@users.noreply.github.com>
2018-10-17 21:18:34 +0200

    Updating

--- MVP.js
+++
@@ -1,7 +0,0 @@
-TriggerRegister.registerChat("MVP").setParameter("contains").setChatCriteria("&b[MVP]").setPriority(Priority.LOWEST);
-
-function MVP(event) {
-  var YTchatMessage = ChatLib.getChatMessage(event, true);
-  ChatLib.chat(YTchatMessage.replace("&b[MVP]", "&b[M]"));
-  cancel(event);
-}
