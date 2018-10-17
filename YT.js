a6cf00af61bc8b8e5661f03aa960500aea4541f5
IconPippi <39626081+IconPippi@users.noreply.github.com>
2018-10-17 21:19:14 +0200

    Updating

--- YT.js
+++
@@ -1,7 +0,0 @@
-TriggerRegister.registerChat("newYT").setParameter("contains").setChatCriteria("&c[&fYOUTUBE&c]").setPriority(Priority.LOWEST);
-
-function newYT(event) {
-  var YTchatMessage = ChatLib.getChatMessage(event, true);
-  ChatLib.chat(YTchatMessage.replace("&c[&fYOUTUBE&c]", "&c[&fYT&c]"));
-  cancel(event);
-}
