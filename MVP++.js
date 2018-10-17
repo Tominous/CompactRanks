1d26e921b0f94469668bc6e4d3de9d6c91f5e667
IconPippi <39626081+IconPippi@users.noreply.github.com>
2018-10-17 21:18:16 +0200

    Updating

--- MVP++.js
+++
@@ -1,7 +0,0 @@
-TriggerRegister.registerChat("MVPPlusPlus").setParameter("contains").setChatCriteria("&6[MVP${color}++${suffix}]").setPriority(Priority.LOWEST);
-
-function MVPPlusPlus(color, suffix, event) {
-  var chatMessage = ChatLib.getChatMessage(event, true);
-  ChatLib.chat(chatMessage.replace("&6[MVP" + color + "++" + suffix + "]", "&r&6[M" + color + "++&6]"));
-  cancel(event);
-}
