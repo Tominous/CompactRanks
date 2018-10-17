8ffa315f59c29b68ed33caac3083b4cb114eca22
IconPippi <39626081+IconPippi@users.noreply.github.com>
2018-10-17 21:17:52 +0200

    Updating

--- HELPER.js
+++
@@ -1,7 +0,0 @@
-TriggerRegister.registerChat("HELPER").setParameter("contains").setChatCriteria("&9[HELPER]").setPriority(Priority.LOWEST);
-
-function HELPER(event) {
-  var YTchatMessage = ChatLib.getChatMessage(event, true);
-  ChatLib.chat(YTchatMessage.replace("&9[HELPER]", "&9[H]"));
-  cancel(event);
-}
