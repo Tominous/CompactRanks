663d25412466b48baf113164c732786a54de8c90
IconPippi <39626081+IconPippi@users.noreply.github.com>
2018-10-17 21:19:05 +0200

    Updating

--- VIP.js
+++
@@ -1,7 +0,0 @@
-TriggerRegister.registerChat("VIP").setParameter("contains").setChatCriteria("&a[VIP]").setPriority(Priority.LOWEST);
-
-function VIP(event) {
-  var YTchatMessage = ChatLib.getChatMessage(event, true);
-  ChatLib.chat(YTchatMessage.replace("&a[VIP]", "&a[V]"));
-  cancel(event);
-}
