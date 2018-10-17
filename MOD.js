fdc1c436e1115e98b81f2bcbd8a1ce854e4c60b6
IconPippi <39626081+IconPippi@users.noreply.github.com>
2018-10-17 21:18:01 +0200

    Updating

--- MOD.js
+++
@@ -1,7 +0,0 @@
-TriggerRegister.registerChat("MOD").setParameter("contains").setChatCriteria("&2[MOD]").setPriority(Priority.LOWEST);
-
-function MOD(event) {
-  var YTchatMessage = ChatLib.getChatMessage(event, true);
-  ChatLib.chat(YTchatMessage.replace("&2[MOD]", "&2[M]"));
-  cancel(event);
-}
