c10ea752c0f4de07a31c25ad481030fc7884b5d4
IconPippi <39626081+IconPippi@users.noreply.github.com>
2018-10-17 21:17:32 +0200

    Updating

--- ADMIN.js
+++
@@ -1,7 +0,0 @@
-TriggerRegister.registerChat("ADMIN").setParameter("contains").setChatCriteria("&c[ADMIN]").setPriority(Priority.LOWEST);
-
-function ADMIN(event) {
-  var YTchatMessage = ChatLib.getChatMessage(event, true);
-  ChatLib.chat(YTchatMessage.replace("&c[ADMIN]", "&c[A]"));
-  cancel(event);
-}
